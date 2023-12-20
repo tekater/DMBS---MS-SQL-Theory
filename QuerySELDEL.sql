325e7550bbcb4c6a11f81b817472678bae695541
tekater <120014627+tekater@users.noreply.github.com>
2023-12-20 23:13:57 +0900

    Create QuerySELDEL.sql

---
+++ QuerySELDEL.sql
@@ -0,0 +1,19 @@
+--DELETE FROM Schedule Where discipline = 14;
+
+SELECT 
+	dbo.Disciplines.discipline_name AS 'Дисциплина',
+	dbo.Groups.group_name			AS 'Группа',
+	dbo.Schedule.[date]				AS 'Дата',
+	dbo.Schedule.[time]				AS 'Время',
+	dbo.Teachers.last_name	+	' ' +
+	dbo.Teachers.first_name +	' ' +
+	dbo.Teachers.middle_name		AS 'Преподаватель',
+	dbo.Schedule.spent				AS 'Проведено' 
+FROM
+	dbo.Schedule,dbo.Groups,dbo.Disciplines,dbo.Teachers
+WHERE
+	dbo.Schedule.discipline = dbo.Disciplines.disciplines_id
+AND
+	dbo.Schedule.[group]	= dbo.Groups.group_id
+AND
+	dbo.Schedule.teacher	= dbo.Teachers.teacher_id;
