ae832e5c32d3393425671b2150fd396db76787ad
tekater <120014627+tekater@users.noreply.github.com>
2023-12-22 23:21:02 +0900

    Update Query2_SELDEL.sql

--- Query2_SELDEL.sql
+++ Query2_SELDEL.sql
@@ -1,14 +1,18 @@
+USE DBMS;
+
 --DELETE FROM Schedule Where discipline = 14;
 
 SELECT 
 	dbo.Disciplines.discipline_name AS 'Дисциплина',
 	dbo.Groups.group_name			AS 'Группа',
+	DATENAME(dw,dbo.Schedule.[date])AS 'День недели',
 	dbo.Schedule.[date]				AS 'Дата',
 	dbo.Schedule.[time]				AS 'Время',
 	dbo.Teachers.last_name	+	' ' +
 	dbo.Teachers.first_name +	' ' +
 	dbo.Teachers.middle_name		AS 'Преподаватель',
-	dbo.Schedule.spent				AS 'Проведено' 
+	[Статус Занятия]				= IIF(dbo.Schedule.spent = 1,'Проведено','Запланировано')
+	 
 FROM
 	dbo.Schedule,dbo.Groups,dbo.Disciplines,dbo.Teachers
 WHERE
