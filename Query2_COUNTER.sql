65337a4c118f4f08658e195bb458aed77221b712
tekater <120014627+tekater@users.noreply.github.com>
2023-12-22 23:47:22 +0900

    COUNTER DONE

--- Query2_COUNTER.sql
+++ Query2_COUNTER.sql
@@ -1,8 +1,34 @@
 USE DBMS;
 
-SELECT COUNT(*)
+SELECT
+	COUNT(*) AS 'Всего',
+	sum(case when [spent] = 1 then 1 else 0 end) AS 'Проведено',
+	sum (case when [spent] = 0 then 1 else 0 end) AS 'Запланировано'
+
 FROM Schedule
 WHERE
 	[group]			= (SELECT group_id FROM Groups WHERE group_name = 'PD_212')
 AND	
 	[discipline]	= (SELECT disciplines_id FROM Disciplines WHERE discipline_name LIKE '%MS SQL%')
+
+
+--SELECT 
+--	COUNT(*) AS 'Всего'
+--FROM Schedule
+--WHERE
+--	[group]			= (SELECT group_id FROM Groups WHERE group_name = 'PD_212')
+--AND	
+--	[discipline]	= (SELECT disciplines_id FROM Disciplines WHERE discipline_name LIKE '%MS SQL%')
+
+
+	
+--SELECT 
+--	COUNT(*) AS 'Проведено'
+--FROM Schedule
+--WHERE
+--	[group]			= (SELECT group_id FROM Groups WHERE group_name = 'PD_212')
+--AND	
+--	[discipline]	= (SELECT disciplines_id FROM Disciplines WHERE discipline_name LIKE '%MS SQL%')
+--AND
+--	[spent]			= 1
+	
