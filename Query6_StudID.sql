f5bbcfcd56b166a6639902f6530e217ba10d2ec0
tekater <120014627+tekater@users.noreply.github.com>
2023-12-27 23:57:31 +0900

    Update Query6_StudID.sql

--- Query6_StudID.sql
+++ Query6_StudID.sql
@@ -2,8 +2,8 @@ USE DBMS;
 
 SELECT 
 	Students.stud_id AS 'ID',
-	FORMATMESSAGE('%s %s %s', last_name, firts_name, middle_name) AS 'Ñòóäåíò',
-	group_name AS 'Ãðóïïà'
+	FORMATMESSAGE('%s %s %s', last_name, firts_name, middle_name) AS 'Студент',
+	group_name AS 'Группа'
 FROM 
 	Students RIGHT JOIN Groups ON [group] = group_id
 WHERE
